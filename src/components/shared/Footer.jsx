a82c5e96e9d7f5130fb71c008c0acb4c8ffe029d
Maxi Ordoñez <exemaxi@outlook.com>
2025-10-01 21:59:44 -0300

    Se agrego clases css a los iconos de redes

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -7,20 +7,20 @@ export const Footer = () => {
         <Row>
           {/* Columna 1 */}
           <Col md={4} className="mb-3 text-center text-md-start">
-            <h5>Recursos</h5>
+            <h5>Categorias</h5>
             <ul className="list-unstyled">
               <li>
-                <a href="#" className="text-light text-decoration-none">
+                <a href="#" className="text-light text-decoration-none ftCss">
                   Preguntas Frecuentes
                 </a>
               </li>
               <li>
-                <a href="#" className="text-light text-decoration-none">
+                <a href="#" className="text-light text-decoration-none ftCss">
                   Configuración
                 </a>
               </li>
               <li>
-                <a href="#" className="text-light text-decoration-none">
+                <a href="#" className="text-light text-decoration-none ftCss">
                   Tareas
                 </a>
               </li>
@@ -32,12 +32,13 @@ export const Footer = () => {
             <h5>Contáctanos</h5>
             <ul className="list-unstyled">
               <li>
-                <a href="mailto:soporte@gestortareas.com" className="text-light text-decoration-none">
-                  <i className="bi bi-envelope-at-fill"></i> soporte@gestortareas.com
+                <a className="text-light text-decoration-none ftCss">
+                  <i className="bi bi-envelope-at-fill"></i>{" "}
+                  soporte@gestortareas.com
                 </a>
               </li>
               <li>
-                <a href="tel:+541112345678" className="text-light text-decoration-none">
+                <a className="text-light text-decoration-none ftCss">
                   <i className="bi bi-whatsapp"></i> +54 11 1234-5678
                 </a>
               </li>
@@ -51,22 +52,22 @@ export const Footer = () => {
             <ul className="list-unstyled d-flex justify-content-center justify-content-md-start">
               <li className="px-2">
                 <a href="#" className="text-light">
-                  <i className="bi bi-instagram fs-4"></i>
+                  <i className="bi bi-instagram fs-4 social-icon-instagram"></i>
                 </a>
               </li>
               <li className="px-2">
                 <a href="#" className="text-light">
-                  <i className="bi bi-youtube fs-4"></i>
+                  <i className="bi bi-youtube fs-4 social-icon-youtube"></i>
                 </a>
               </li>
               <li className="px-2">
                 <a href="#" className="text-light">
-                  <i className="bi bi-twitter fs-4"></i>
+                  <i className="bi bi-twitter fs-4 social-icon-twitter"></i>
                 </a>
               </li>
               <li className="px-2">
                 <a href="#" className="text-light">
-                  <i className="bi bi-facebook fs-4"></i>
+                  <i className="bi bi-facebook fs-4 social-icon-facebook"></i>
                 </a>
               </li>
             </ul>
@@ -75,10 +76,10 @@ export const Footer = () => {
 
         <Row className="mt-3">
           <Col className="text-center border-1 border-top">
-            <small>
-              © {new Date().getFullYear()} Gestor de Tareas - Todos los derechos
+            <p>
+              &copy; 2025 Gestor de Tareas - Todos los derechos
               reservados
-            </small>
+            </p>
           </Col>
         </Row>
       </Container>
