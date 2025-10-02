da11763f6e5626696a9f273f8fbac024ee4904fd
Maxi Ordoñez <exemaxi@outlook.com>
2025-10-01 22:36:30 -0300

    Se modifico texto en Cards

--- src/components/TareasCards.jsx
+++ src/components/TareasCards.jsx
@@ -5,11 +5,12 @@ export const TareasCards = () => {
   return (
     <Card className="shadow w-100 my-1">
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Title>Titulo: Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Descripcion: Card Subtitle</Card.Subtitle>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Fecha de creacion: Card Text
+        <br />
+        Fecha de modificacion: Card Text
         </Card.Text>
       </Card.Body>
       <CardFooter className="d-flex justify-content-end">
